b27f9a85e4efa32b61e4dcf51c8920bf01ff352c
Sergey Volkov <js2me@outlook.com>
2021-09-25 11:55:35 +0300

    bump: 1.0.13; chore: revert to 1.0.8

--- postinstall.js
+++ postinstall.js
@@ -5,4 +5,4 @@ const path = require('path')
 const pathToInstall = path.resolve(__dirname, '../..')
 const deps = Object.keys(eslintDeps).map(key => `${key}@${eslintDeps[key]}`).join(" ");
 
-exec(`npm install --prefix ${pathToInstall} --no-save --quiet --no-audit ${deps}`, { cwd: process.cwd() }, () => {})
+// exec(`npm install --prefix ${pathToInstall} --no-save --quiet --no-audit ${deps}`, { cwd: process.cwd() }, () => {})
